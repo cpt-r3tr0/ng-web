2596478904a1b2639d6bc8345260f32d01c95551
Rupesh Boridya <rbordiya22@hotmail.com>
2019-08-25 17:05:22 +0530

    minor update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialslModule } from './materials/materials.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialslModule
   ],
   providers: [],
   bootstrap: [AppComponent]
